ad2c03a5aa58e30cdf0b16f944d87555f4941b37
Adesh Pol <adeshpol1234@gmail.com>
2026-07-16 20:52:46 +0530

    Create 03-SynchronizedBlockExample.java

---
+++ 01-MultiThreading/03-Synchronization/03-SynchronizedBlockExample.java
@@ -0,0 +1,45 @@
+package Multithreading;
+
+class Counter {
+    int count = 0;
+
+    public void increment() {
+
+        synchronized (this) {
+            count++;
+        }
+    }
+}
+
+public class SynchronizedBlockExample {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Counter counter = new Counter();
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 1; i <= 1000; i++) {
+                counter.increment();
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 1; i <= 1000; i++) {
+                counter.increment();
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println("Final Count: " + counter.count);
+    }
+}
+
+/*
+Expected Output:
+Final Count: 2000
+*/
