04e085bf635b998097a3ea178d408b1ffcdf38f1
Adesh Pol <adeshpol1234@gmail.com>
2026-07-16 20:39:26 +0530

    Update ThreadClassExample.java

--- 01-MultiThreading/01-Thread_creation/ThreadClassExample.java
+++ 01-MultiThreading/01-Thread_creation/ThreadClassExample.java
@@ -5,7 +5,6 @@ class MyThread extends Thread {
 
     @Override
     public void run() {
-        // Code executed by thread
         for (int i = 1; i <= 5; i++) {
             System.out.println("Thread running: " + i);
         }
@@ -17,12 +16,35 @@ public class ThreadClassExample {
     public static void main(String[] args) {
 
         MyThread t1 = new MyThread();
+        t1.start(); // starts new thread
 
-        t1.start(); // starts new thread (IMPORTANT)
-
-        // main thread
         for (int i = 1; i <= 5; i++) {
             System.out.println("Main thread: " + i);
         }
     }
 }
+
+/*
+Possible Output (order may vary):
+
+Main thread: 1
+Thread running: 1
+Main thread: 2
+Thread running: 2
+Main thread: 3
+Thread running: 3
+Main thread: 4
+Thread running: 4
+Main thread: 5
+Thread running: 5
+
+OR
+
+Thread running: 1
+Thread running: 2
+Main thread: 1
+Main thread: 2
+...
+
+👉 Output is NOT fixed because threads run concurrently
+*/
